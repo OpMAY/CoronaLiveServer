b1810e841b6c78c40db01ab6b7b3872f841d13e8
OpMAY <65110458+OpMAY@users.noreply.github.com>
2020-12-15 15:37:10 +0900

    Initial commit

---
+++ src/test/kotlin/com/application/CoronaLiveServer/CoronaLiveServerApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.application.CoronaLiveServer
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class CoronaLiveServerApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
