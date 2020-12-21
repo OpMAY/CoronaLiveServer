199fb6c640d52aede60515d07b44fba46e75c047
OpMAY <65110458+OpMAY@users.noreply.github.com>
2020-12-21 14:05:15 +0900

    서버 원상복구

---
+++ src/main/kotlin/com/application/coronaliveserver/CoronaLiveServerApplication.kt
@@ -0,0 +1,11 @@
+package com.application.CoronaLiveServer
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class CoronaLiveServerApplication
+
+fun main(args: Array<String>) {
+	runApplication<CoronaLiveServerApplication>(*args)
+}
